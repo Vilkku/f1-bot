4919a313ed9517fcb2bcbb1f44517b6cd3869707
Vilkku <vilkku@gmail.com>
2013-10-05 23:41:05 +0300

    Add config file, remove sensitive options from main file

--- f1-bot.py
+++ f1-bot.py
@@ -2,9 +2,10 @@ import praw
 import mysql.connector
 import re
 import datetime
+import json
+from os import path
 
 # Functions
-
 def setTag(keyword, content, sidebar):
     p = re.compile('\[\]\(\/f1bot-'+keyword+'-s\)(.*)\[\]\(\/f1bot-'+keyword+'-e\)', re.IGNORECASE)
        
@@ -14,11 +15,10 @@ def setTag(keyword, content, sidebar):
     return sidebar
     
 def getCountdownTime():
-    sessions = ['Practice 1', 'Practice 2', 'Practice 3', 'Qualifying', 'Race', 'Hungary Practice 1']
-    times = ['2013-07-05 11:00', '2013-07-05 15:00', '2013-07-06 12:00', '2013-07-06 15:00', '2013-07-07 15:00', '2013-07-26 11:00']
+    sessions = ['Practice 1', 'Practice 2', 'Practice 3', 'Qualifying', 'Race', 'Japan Practice 1']
+    times = ['2013-10-04 04:00', '2013-10-04 08:00', '2013-10-05 05:00', '2013-10-05 08:00', '2013-10-06 09:00', '2013-10-11 04:00']
     lengths = [90, 90, 60, 90, 120, 90]
-    i = 0
-    for session in sessions:
+    for (i, session) in enumerate(sessions):
         sessiontime = datetime.datetime.strptime(times[i], '%Y-%m-%d %H:%M')
         sessionlength = datetime.timedelta(0, lengths[i]*60)
         timeleft = sessiontime - datetime.datetime.now()
@@ -31,22 +31,21 @@ def getCountdownTime():
         # If the event time has passed but the event isn't over
         if (timeleft + sessionlength) > datetime.timedelta():
             return "Live!"
-            
-        i += 1
+
+# Read configuration file
+dir = path.dirname(__file__)
+config_data = open(path.join(dir, "config.json"), "r")
+config = json.load(config_data)
+config_data.close()
 
 # Reddit login
-
-username = "" #There should probably be accepted as arguments to the script instead
-password = ""
-r = praw.Reddit(user_agent='F1-Bot (/u/F1-Bot) for /r/formula1 by /u/vilkku')
-r.login(username, password)
+r = praw.Reddit(config['reddit']['user_agent'])
+r.login(config['reddit']['username'], config['reddit']['password'])
 
 # Connect to MySQL database
-
-cnx = mysql.connector.connect(user='', password='', host='', database='', buffered=True)
+cnx = mysql.connector.connect(user=config['mysql']['username'].encode(), password=config['mysql']['password'].encode(), host=config['mysql']['host'].encode(), database=config['mysql']['database'].encode(), buffered=True, collation='utf8_swedish_ci', charset='utf8')
 
 # Search for rows where the difference between schedule and now is less than 3 hours
-
 cursor = cnx.cursor()
 updateCursor = cnx.cursor()
 fetch_query = ("SELECT id, subreddit, title, text FROM f1_bot WHERE (TIME_TO_SEC(TIMEDIFF(schedule, NOW())) < 300) AND (posted = 0)")
@@ -58,13 +57,13 @@ if (cursor.rowcount > 0):
         s = r.submit(subreddit, title, text=text)
         s.set_flair(flair_css_class='star')
         s.distinguish()
+        s.sticky()
         updateCursor.execute(update_query, { 'post_id': id })
 
 cursor.close()
 updateCursor.close()
 
 # Update the sidebar
-
 current_sidebar = r.get_subreddit("formula1").get_settings()["description"]
 new_sidebar = current_sidebar
     
@@ -72,5 +71,4 @@ new_sidebar = setTag("countdown", getCountdownTime(), new_sidebar)
 r.get_subreddit("formula1").update_settings(description=new_sidebar)
 
 # Close the MySQL connection
-
 cnx.close()
---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `f1_bot` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `subreddit` text COLLATE utf8_swedish_ci NOT NULL,
+  `title` text COLLATE utf8_swedish_ci NOT NULL,
+  `text` text COLLATE utf8_swedish_ci NOT NULL,
+  `schedule` datetime NOT NULL,
+  `posted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
