58aeda259cce3ec9069a8ea00f592f02559d79d7
Wilhelm Törnroos <vilkku@gmail.com>
2013-11-24 20:19:49 +0200

    Add weeks and months eft to countdown message

--- f1-bot.py
+++ f1-bot.py
@@ -35,7 +35,7 @@ def getCountdownTime():
             # If the event time hasn't passed
             if timeleft > datetime.timedelta():
                 d = datetime.datetime(1,1,1) + timeleft
-                return ("**%s**: %dD %dH %dM" % (session, d.day-1, d.hour, d.minute))
+                return ("**%s**: %dM %dW %dD %dH %dM" % (session, d.month, d.week, d.day-1, d.hour, d.minute))
                 
             # If the event time has passed but the event isn't over
             if (timeleft + sessionlength) > datetime.timedelta():
