f38ad8cdc5c9f4272f04f9801cde3f0d6d46fec6
Vilkku <Vilkku@users.noreply.github.com>
2014-06-10 10:23:11 +0300

    Update schema.sql
    Added flair text and CSS columns

--- schema.sql
+++ schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS `f1_bot` (
   `subreddit` text COLLATE utf8_swedish_ci NOT NULL,
   `title` text COLLATE utf8_swedish_ci NOT NULL,
   `text` text COLLATE utf8_swedish_ci NOT NULL,
+  `flair_text` varchar(255) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
+  `flair_css` varchar(255) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
   `schedule` datetime NOT NULL,
   `posted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
