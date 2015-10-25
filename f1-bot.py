26058a2b4b4a1acafbaaba81a2381505a77bde5e
Wilhelm Tornroos <vilkku@gmail.com>
2015-10-25 15:13:00 +0200

    Add oauth support, improve main functions

--- f1-bot.py
+++ f1-bot.py
@@ -1,13 +1,59 @@
-import praw
-import mysql.connector
-import re
-import datetime
 from collections import OrderedDict
-import json
-import HTMLParser
 from os import path
 
-# Functions
+import datetime
+import HTMLParser
+import json
+import mysql.connector
+import OAuth2Util
+import praw
+import re
+
+def main():
+    # Read configuration files
+    dir = path.dirname(__file__)
+
+    config_data = open(path.join(dir, "config.json"), "r")
+    config = json.load(config_data)
+    config_data.close()
+
+    sessions_data = open(path.join(dir, "schedule.json"), "r")
+    sessions = json.load(sessions_data, object_pairs_hook=OrderedDict)
+    sessions_data.close()
+
+    # Reddit login
+    r = praw.Reddit(config['reddit']['user_agent'])
+    o = OAuth2Util.OAuth2Util(r)
+    o.refresh(force=True)
+
+    if (mysql in config):
+        # Connect to MySQL database
+        cnx = mysql.connector.connect(
+            user=config['mysql']['username'].encode(),
+            password=config['mysql']['password'].encode(),
+            host=config['mysql']['host'].encode(),
+            database=config['mysql']['database'].encode(),
+            buffered=True,
+            collation='utf8_swedish_ci',
+            charset='utf8'
+        )
+
+        postScheduledPosts(cnx, r)
+
+        # Close the MySQL connection
+        cnx.close()
+
+    # Update the sidebar
+    h = HTMLParser.HTMLParser()
+    current_sidebar = r.get_subreddit(config['reddit']['subreddit']).get_settings()["description"]
+    current_sidebar = h.unescape(current_sidebar)
+    new_sidebar = current_sidebar
+
+    new_sidebar = setTag("countdown", getCountdownTime(sessions), new_sidebar)
+    r.get_subreddit(config['reddit']['subreddit']).update_settings(description=new_sidebar)
+
+# Take a sidebar, find [](/f1bot-keyword-s)<*>[](/f1bot-keyword-e), replace with
+# [](/f1bot-keyword-s)<content>[](/f1bot-keyword-e), return updated sidebar
 def setTag(keyword, content, sidebar):
     p = re.compile('\[\]\(\/f1bot-'+keyword+'-s\)(.*)\[\]\(\/f1bot-'+keyword+'-e\)', re.IGNORECASE)
 
@@ -16,11 +62,10 @@ def setTag(keyword, content, sidebar):
             sidebar = sidebar.replace(match.group(0), '[](/f1bot-'+keyword+'-s)'+content+' [](/f1bot-'+keyword+'-e)')
     return sidebar
 
-def getCountdownTime():
-    dir = path.dirname(__file__)
-    sessions_data = open(path.join(dir, "schedule.json"), "r")
-    sessions = json.load(sessions_data, object_pairs_hook=OrderedDict)
-    sessions_data.close()
+# Take session data, figure out what the next session is and return a formatted
+# string saying how much time is left until that session begins. If the session
+# is in progress, return a string stating that.
+def getCountdownTime(sessions):
     lengths = {
         'Practice 1': 90,
         'Practice 2': 90,
@@ -37,8 +82,14 @@ def getCountdownTime():
             # If the event time hasn't passed
             if timeleft > datetime.timedelta():
                 d = datetime.datetime(1,1,1) + timeleft
-                return ("**%s**: %dD %dH %dM" % (session, d.day-1, d.hour, d.minute))
-                #return ("**%s**: %dM %dD" % (session, d.month-1, d.day-1))
+                if (d.hour < 24):
+                    return ("**%s**: %dH %dM" % (session, d.hour, d.minute))
+                elif (d.day < 7):
+                    return ("**%s**: %dD %dH" % (session, d.day-1, d.hour))
+                elif (d.day < 30):
+                    return ("**%s**: %dD" % (session, d.day-1))
+                else:
+                    return ("**%s**: %dM %dD" % (session, d.month-1, d.day-1))
 
             # If the event time has passed but the event isn't over
             if (timeleft + sessionlength) > datetime.timedelta():
@@ -46,55 +97,31 @@ def getCountdownTime():
 
     return ""
 
-# Read configuration file
-dir = path.dirname(__file__)
-config_data = open(path.join(dir, "config.json"), "r")
-config = json.load(config_data)
-config_data.close()
+# Take a database connection and a reddit login, figure out if there are rows in the f1_bot table that have a
+# scheduled time in the past and submit those posts to reddit according to the information in the table. If
+# the logged in user has moderator powers, distinguish, add flair, sticky and post a comment.
+def postScheduledPosts(cnx, reddit):
+    # Search for rows where the difference between schedule and now is less than 3 hours
+    cursor = cnx.cursor()
+    updateCursor = cnx.cursor()
+    fetch_query = ("SELECT id, subreddit, title, text, flair_text, flair_css FROM f1_bot WHERE (TIME_TO_SEC(TIMEDIFF(schedule, NOW())) < 300) AND (posted = 0)")
+    update_query = ("UPDATE f1_bot SET posted=1 WHERE id=%(post_id)s")
+    cursor.execute(fetch_query)
 
-# Reddit login
-r = praw.Reddit(config['reddit']['user_agent'])
-r.login(config['reddit']['username'], config['reddit']['password'])
+    if (cursor.rowcount > 0):
+        for (post_id, subreddit, title, text, flair_text, flair_css) in cursor:
+            s = reddit.submit(subreddit, title, text=text)
+            updateCursor.execute(update_query, {'post_id': post_id})
+            cnx.commit()
 
-# Connect to MySQL database
-cnx = mysql.connector.connect(
-    user=config['mysql']['username'].encode(),
-    password=config['mysql']['password'].encode(),
-    host=config['mysql']['host'].encode(),
-    database=config['mysql']['database'].encode(),
-    buffered=True,
-    collation='utf8_swedish_ci',
-    charset='utf8')
+            moderators = reddit.get_subreddit(subreddit).get_moderators()
+            if any(x for x in moderators if x.name == reddit.user.name):
+                s.set_flair(flair_text=flair_text, flair_css_class=flair_css)
+                s.distinguish()
+                s.sticky()
+                s.add_comment('Please post streams and stream requests as a reply to this comment.')
 
-# Search for rows where the difference between schedule and now is less than 3 hours
-cursor = cnx.cursor()
-updateCursor = cnx.cursor()
-fetch_query = ("SELECT id, subreddit, title, text, flair_text, flair_css FROM f1_bot WHERE (TIME_TO_SEC(TIMEDIFF(schedule, NOW())) < 300) AND (posted = 0)")
-update_query = ("UPDATE f1_bot SET posted=1 WHERE id=%(post_id)s")
-cursor.execute(fetch_query)
+    cursor.close()
+    updateCursor.close()
 
-if (cursor.rowcount > 0):
-    for (post_id, subreddit, title, text, flair_text, flair_css) in cursor:
-        s = r.submit(subreddit, title, text=text)
-        updateCursor.execute(update_query, { 'post_id': post_id })
-        cnx.commit()
-        if (subreddit == 'formula1' or subreddit == 'vilkku'):
-            s.set_flair(flair_text=flair_text,flair_css_class=flair_css)
-            s.distinguish()
-            s.sticky()
-            s.add_comment('Please post streams and stream requests as a reply to this comment.')
-
-cursor.close()
-updateCursor.close()
-
-# Update the sidebar
-h = HTMLParser.HTMLParser()
-current_sidebar = r.get_subreddit("formula1").get_settings()["description"]
-current_sidebar = h.unescape(current_sidebar)
-new_sidebar = current_sidebar
-
-new_sidebar = setTag("countdown", getCountdownTime(), new_sidebar)
-r.get_subreddit("formula1").update_settings(description=new_sidebar)
-
-# Close the MySQL connection
-cnx.close()
+if __name__ == "__main__": main()
